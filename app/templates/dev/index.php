f284655e053143489e9109cfe80db2554cc9ba47
xuzhanfu <terry@fpi.cc>
2014-03-23 21:03:04 +0800

    change a spell error

--- app/templates/dev/index.php
+++ app/templates/dev/index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-<title>Ncik Site Template</title>
+<title>Nick Site Template</title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 <link href="styles/style.css" rel="stylesheet">
